afbc9377b5a2673d87c1eb12ba1a459d465b707a
Homyachok <kibenko.ekaterina@gmail.com>
2022-07-15 00:23:19 +0300

    Решили 2 задачу

---
+++ HW002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №2. 
+
+int a = new Random().Next(10,20);
+int b = new Random().Next(10,20);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+
+else
+{
+    Console.WriteLine(b);
+}
