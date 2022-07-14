06dbf62640489eed573369df50091b8719635699
Homyachok <kibenko.ekaterina@gmail.com>
2022-07-15 01:05:17 +0300

    Решили задачу №8

---
+++ HW008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №8
+
+double N = new Random().Next(10,20);
+double a = 1;
+Console.WriteLine("N = " + N);
+while (a < N)
+{
+    if (a%2==0) Console.Write(a + ", ");
+    a++;
+}
+Console.WriteLine("END");
