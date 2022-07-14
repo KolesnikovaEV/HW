2c201f8260c4d4826b739da6c6ad0c47bbf81ecd
Homyachok <kibenko.ekaterina@gmail.com>
2022-07-15 00:44:16 +0300

    Решили 4 задачу

---
+++ HW004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №4
+
+int a = new Random().Next(10,20);
+int b = new Random().Next(10,20);
+int c = new Random().Next(10,20);
+int max = a;
+
+Console.WriteLine(a + ", " + b + ", " + c);
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine(max);
